133aee3717e124a2ddfc57f7b58b2deb9e083ccd
Svitlan Kueng <svity@Svitlans-MacBook-Air.local>
2026-02-01 15:58:13 +0100

    Listing the things to do

--- todolist.py
+++ todolist.py
@@ -1,10 +1,28 @@
 import json
 
+
+def listTask():
+    with open("data.json") as data_file:
+        data = json.load(data_file)
+        for task in data["tasks"]:
+            print(task)
+
+
+
 def saveToJson():
-    tasks = input("Please input your tasks:")
-    data = {"Task": tasks}
+    task = input("Please input your tasks:")
+    
+    try:
+        #loads data.json file with the tasks to prevent from creating a new file everytime
+        with open("data.json") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {"tasks": []}
+
+    data["tasks"].append(task)
+
     with open("data.json", "w") as f:
         json.dump(data, f)
     print("Saved to json file")
-
 saveToJson()
+listTask()
